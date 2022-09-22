779b7995d017016b8cc74aac0b7708923e2afd86
X_sisiphus <1821570960@qq.com>
2022-09-22 14:10:19 +0800

    firstApp

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: SafeArea(
+        child:const WebView(
+        initialUrl: 'http://localhost:8080/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+      )
+    );
+  }
+}
